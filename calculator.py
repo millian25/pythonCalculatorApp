ccfc5e3db56a7456513bb537556c6297c8765a4b
Mille Wanyanga <millianmille11@gmail.com>
2024-10-21 17:33:45 +0300

    changes made to the original file

--- calculator.py
+++ calculator.py
@@ -19,29 +19,34 @@ def divide(x ,y):
         return x/y
     
     #Main Program
-    
-# User Input
-x = float(input("Enter the value of x: "))
-y = float(input("Enter the value of y: "))
-    
-#Display operations
-print("Select operation: ")
-print("1. Add")
-print("2. Substract")
-print("3. Multipy")
-print("4. Divide")
+while True:
+    # User Input
+    x = float(input("Enter the value of x: "))
+    y = float(input("Enter the value of y: "))
+        
+    #Display operations
+    print("Select operation: ")
+    print("1. Add")
+    print("2. Substract")
+    print("3. Multipy")
+    print("4. Divide")
 
-#Collecting user input for operation
-operation = input("Enter your choice (1/2/3/4): ")
+    #Collecting user input for operation
+    operation = input("Enter your choice (1/2/3/4): ")
 
-#Dealing with conditionals
-if operation == "1":
-    print(f"The result of {x} + {y} is : {add(x,y)}")
-elif operation == "2":
-    print(f"The result of {x} - {y} is: {substract(x,y)}")
-elif operation == "3":
-    print(f"The result of {x} * {y} is: {multiply(x,y)}")
-elif operation =="4":
-    print(f"The result of {x} / {y} is: {divide(x,y)}")
-else:
-    print("Invalid input! Please choose a valid operation")
+    #Dealing with conditionals
+    if operation == "1":
+        print(f"The result of {x} + {y} is : {add(x,y)}")
+    elif operation == "2":
+        print(f"The result of {x} - {y} is: {substract(x,y)}")
+    elif operation == "3":
+        print(f"The result of {x} * {y} is: {multiply(x,y)}")
+    elif operation =="4":
+        print(f"The result of {x} / {y} is: {divide(x,y)}")
+    else:
+        print("Invalid input! Please choose a valid operation")
+
+# Ask user if they want to perfom another calculation
+    continue_calculation = input("Do you want to perform another calculation? yes/no: ")
+    if continue_calculation.lower() != 'yes':
+        break #Exit the loop
