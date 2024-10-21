049d797ff6473ab359abcc7ee173cc623ad0d650
Mille Wanyanga <millianmille11@gmail.com>
2024-10-21 17:07:37 +0300

    Initial commit of the Calculator Program

---
+++ calculator.py
@@ -0,0 +1,47 @@
+# Function for Addition
+def add(x, y):
+    return x + y
+
+#Substarction
+def substract(x,y):
+    return x - y
+
+# Multiplication
+def multiply(x,y):
+    return x * y
+
+# Division
+def divide(x ,y):
+    # To divide by zero
+    if y == 0:
+        return "Error! Division by Zero."
+    else: 
+        return x/y
+    
+    #Main Program
+    
+# User Input
+x = float(input("Enter the value of x: "))
+y = float(input("Enter the value of y: "))
+    
+#Display operations
+print("Select operation: ")
+print("1. Add")
+print("2. Substract")
+print("3. Multipy")
+print("4. Divide")
+
+#Collecting user input for operation
+operation = input("Enter your choice (1/2/3/4): ")
+
+#Dealing with conditionals
+if operation == "1":
+    print(f"The result of {x} + {y} is : {add(x,y)}")
+elif operation == "2":
+    print(f"The result of {x} - {y} is: {substract(x,y)}")
+elif operation == "3":
+    print(f"The result of {x} * {y} is: {multiply(x,y)}")
+elif operation =="4":
+    print(f"The result of {x} / {y} is: {divide(x,y)}")
+else:
+    print("Invalid input! Please choose a valid operation")
